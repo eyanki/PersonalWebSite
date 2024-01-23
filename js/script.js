a9adc728a5043f4de5cc5f9db15af0684c5289bd
eyanki <125137540+eyanki@users.noreply.github.com>
2024-01-23 10:59:54 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-function bigImg(x) {
-    x.style.width = "550px";
+function validateForm() {
+  let x = document.forms["contactForm"]["name"].value;
+  let y = document.forms["contactForm"]["email"].value;
+  let z = document.forms["contactForm"]["tel"].value;
+  let w = document.forms["contactForm"]["description"].value;
+  if (x == "" || y == "" || z == "" || w == "") {
+    alert("Required field must be filled out");
+    return false;
   }
-   
-  function normalImg(x) {
-    x.style.width = "50px";
+  else{
+    document.getElementById("submitButton").setAttribute("data-toggle","modal");
+    document.getElementById("submitButton").setAttribute("data-target","#myModal");
+    return true;
   }
 
- 
+
+}
